4e78aa17991f75d2723d09861cdb02efd05fad05
harshmaniya <harshmaniya201@gmail.com>
2022-08-23 17:50:47 +0530

    practical6

--- src/main/kotlin/practical6.kt
+++ src/main/kotlin/practical6.kt
@@ -1,8 +1,7 @@
 fun main(){
     fun add(vararg x:Int){
-        var add:Int = x[0]
-        for(i in x[1]..x.size){
-            println("Add = $add")
+        var add:Int = 0
+        for(i in x){
             add += i
         }
         var y:String = ""
@@ -13,8 +12,8 @@ fun main(){
     }
 
     fun sub(vararg x:Int){
-        var sub:Int = x[0]
-        for(i in x[1]..x.size-1){
+        var sub:Int = 0
+        for(i in x){
             sub -= i
         }
         var y:String = ""
@@ -25,8 +24,8 @@ fun main(){
     }
 
     fun mul(vararg x:Int){
-        var mul:Int = x[0]
-        for(i in x[1]..x.size){
+        var mul:Int = 1
+        for(i in x){
             mul *= i
         }
         var y:String = ""
@@ -37,19 +36,22 @@ fun main(){
     }
 
     fun div(vararg x:Int) {
-        var div: Double = x[0].toDouble()
-        for (i in x[1]..x.size) {
+        var div:Double = x[0].toDouble()
+        for (i in x) {
+            if(i == x[0]) {
+                continue
+            }
             div /= i
         }
         var y: String = ""
         for (i in x) {
             y += "$i,"
         }
-        println("Divition of $y is $div")
+        println("Division of $y is $div")
     }
 
-    add(1,2,3,1)
+    add(1,1,1,1)
     sub(20,1,1)
-    mul(1,2,3)
-    div(20,2,2)
+    mul(2,2,2,2)
+    div(20,2,2,2)
 }
